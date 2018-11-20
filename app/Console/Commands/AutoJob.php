8ad4781c58828dc7fc684cd22ff294bad6d62726
dcb9 <bob@phpor.me>
2018-11-20 18:24:26 +0800

    Update auto job

--- app/Console/Commands/AutoJob.php
+++ app/Console/Commands/AutoJob.php
@@ -4,14 +4,10 @@ namespace App\Console\Commands;
 
 use App\Components\Helpers;
 use App\Components\Yzy;
-use App\Http\Models\Goods;
-use App\Http\Models\GoodsLabel;
+use App\Components\PaymentComponent;
 use App\Http\Models\ReferralLog;
-use App\Http\Models\SsNode;
-use App\Http\Models\SsNodeLabel;
 use App\Http\Models\UserBalanceLog;
 use App\Http\Models\VerifyCode;
-use App\Mail\sendUserInfo;
 use Illuminate\Console\Command;
 use App\Http\Models\Coupon;
 use App\Http\Models\Invite;
@@ -288,7 +284,7 @@ class AutoJob extends Command
          * 当有赞没有正常推送消息或者其他原因导致用户已付款但是订单不生效从而导致用户无法正常加流量、置状态
          * 故需要每分钟请求一次未支付订单，审计一下其支付状态
          */
-        $paymentList = Payment::query()->with(['order', 'user'])->where('status', 0)->get();
+        $paymentList = Payment::query()->with(['order', 'user'])->where('status', 0)->where('pay_way', 1)->get();
         if (!$paymentList->isEmpty()) {
             foreach ($paymentList as $payment) {
                 // 跳过order丢失的订单
@@ -305,180 +301,10 @@ class AutoJob extends Command
                         continue;
                     }
 
-                    // 处理订单
-                    DB::beginTransaction();
                     try {
-                        // 如果支付单中没有用户信息则创建一个用户
-                        if (!$payment->user_id) {
-                            // 生成一个可用端口
-                            $port = self::$systemConfig['is_rand_port'] ? Helpers::getRandPort() : Helpers::getOnlyPort();
-
-                            $user = new User();
-                            $user->username = '自动生成-' . $payment->order->email;
-                            $user->password = md5(makeRandStr());
-                            $user->port = $port;
-                            $user->passwd = makeRandStr();
-                            $user->vmess_id = createGuid();
-                            $user->enable = 1;
-                            $user->method = Helpers::getDefaultMethod();
-                            $user->protocol = Helpers::getDefaultProtocol();
-                            $user->obfs = Helpers::getDefaultObfs();
-                            $user->usage = 1;
-                            $user->transfer_enable = 1; // 新创建的账号给1，防止定时任务执行时发现u + d >= transfer_enable被判为流量超限而封禁
-                            $user->enable_time = date('Y-m-d');
-                            $user->expire_time = date('Y-m-d', strtotime("+" . $payment->order->goods->days . " days"));
-                            $user->reg_ip = getClientIp();
-                            $user->referral_uid = 0;
-                            $user->traffic_reset_day = 0;
-                            $user->status = 1;
-                            $user->save();
-
-                            if ($user->id) {
-                                Order::query()->where('oid', $payment->oid)->update(['user_id' => $user->id]);
-                            }
-                        }
-
-                        // 更新支付单
-                        $payment->pay_way = $trade['response']['qr_trades']['pay_type'] == 'WXPAY_BIGUNSIGN' ? 1 : 2; // 1-微信、2-支付宝
-                        $payment->status = 1;
-                        $payment->save();
-
-                        // 更新订单
-                        $order = Order::query()->with(['user'])->where('oid', $payment->oid)->first();
-                        $order->status = 2;
-                        $order->save();
-
-                        $goods = Goods::query()->where('id', $order->goods_id)->first();
-
-                        // 商品为流量或者套餐
-                        if ($goods->type <= 2) {
-                            // 如果买的是套餐，则先将之前购买的所有套餐置都无效，并扣掉之前所有套餐的流量，重置用户已用流量为0
-                            if ($goods->type == 2) {
-                                $existOrderList = Order::query()
-                                    ->with(['goods'])
-                                    ->whereHas('goods', function ($q) {
-                                        $q->where('type', 2);
-                                    })
-                                    ->where('user_id', $order->user_id)
-                                    ->where('oid', '<>', $order->oid)
-                                    ->where('is_expire', 0)
-                                    ->where('status', 2)
-                                    ->get();
-
-                                foreach ($existOrderList as $vo) {
-                                    Order::query()->where('oid', $vo->oid)->update(['is_expire' => 1]);
-
-                                    // 先判断，防止手动扣减过流量的用户流量被扣成负数
-                                    if ($order->user->transfer_enable - $vo->goods->traffic * 1048576 <= 0) {
-                                        User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0, 'transfer_enable' => 0]);
-                                    } else {
-                                        User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0]);
-                                        User::query()->where('id', $order->user_id)->decrement('transfer_enable', $vo->goods->traffic * 1048576);
-                                    }
-                                }
-                            }
-
-                            // 把商品的流量加到账号上
-                            User::query()->where('id', $order->user_id)->increment('transfer_enable', $goods->traffic * 1048576);
-
-                            // 计算账号过期时间
-                            if ($order->user->expire_time < date('Y-m-d', strtotime("+" . $goods->days . " days"))) {
-                                $expireTime = date('Y-m-d', strtotime("+" . $goods->days . " days"));
-                            } else {
-                                $expireTime = $order->user->expire_time;
-                            }
-
-                            // 套餐就改流量重置日，流量包不改
-                            if ($goods->type == 2) {
-                                if (date('m') == 2 && date('d') == 29) {
-                                    $traffic_reset_day = 28;
-                                } else {
-                                    $traffic_reset_day = date('d') == 31 ? 30 : abs(date('d'));
-                                }
-
-                                User::query()->where('id', $order->user_id)->update(['traffic_reset_day' => $traffic_reset_day, 'expire_time' => $expireTime, 'enable' => 1]);
-                            } else {
-                                User::query()->where('id', $order->user_id)->update(['expire_time' => $expireTime, 'enable' => 1]);
-                            }
-
-                            // 写入用户标签
-                            if ($goods->label) {
-                                // 用户默认标签
-                                $defaultLabels = [];
-                                if (self::$systemConfig['initial_labels_for_user']) {
-                                    $defaultLabels = explode(',', self::$systemConfig['initial_labels_for_user']);
-                                }
-
-                                // 取出现有的标签
-                                $userLabels = UserLabel::query()->where('user_id', $order->user_id)->pluck('label_id')->toArray();
-                                $goodsLabels = GoodsLabel::query()->where('goods_id', $order->goods_id)->pluck('label_id')->toArray();
-
-                                // 标签去重
-                                $newUserLabels = array_values(array_unique(array_merge($userLabels, $goodsLabels, $defaultLabels)));
-
-                                // 删除用户所有标签
-                                UserLabel::query()->where('user_id', $order->user_id)->delete();
-
-                                // 生成标签
-                                foreach ($newUserLabels as $vo) {
-                                    $obj = new UserLabel();
-                                    $obj->user_id = $order->user_id;
-                                    $obj->label_id = $vo;
-                                    $obj->save();
-                                }
-                            }
-
-                            // 写入返利日志
-                            if ($order->user->referral_uid) {
-                                $this->addReferralLog($order->user_id, $order->user->referral_uid, $order->oid, $order->amount, $order->amount * self::$systemConfig['referral_percent']);
-                            }
-
-                            // 取消重复返利
-                            User::query()->where('id', $order->user_id)->update(['referral_uid' => 0]);
-
-                        } elseif ($goods->type == 3) { // 商品为在线充值
-                            User::query()->where('id', $order->user_id)->increment('balance', $goods->price * 100);
-
-                            // 余额变动记录日志
-                            $this->addUserBalanceLog($order->user_id, $order->oid, $order->user->balance, $order->user->balance + $goods->price, $goods->price, '用户在线充值');
-                        }
-
-                        // 自动提号机：如果order的email值不为空
-                        if ($order->email) {
-                            $title = '【' . self::$systemConfig['website_name'] . '】您的账号信息';
-                            $content = [
-                                'order_sn'      => $order->order_sn,
-                                'goods_name'    => $order->goods->name,
-                                'goods_traffic' => flowAutoShow($order->goods->traffic * 1048576),
-                                'port'          => $order->user->port,
-                                'passwd'        => $order->user->passwd,
-                                'method'        => $order->user->method,
-                                //'protocol'       => $order->user->protocol,
-                                //'protocol_param' => $order->user->protocol_param,
-                                //'obfs'           => $order->user->obfs,
-                                //'obfs_param'     => $order->user->obfs_param,
-                                'created_at'    => $order->created_at->toDateTimeString(),
-                                'expire_at'     => $order->expire_at
-                            ];
-
-                            // 获取可用节点列表
-                            $labels = UserLabel::query()->where('user_id', $order->user_id)->get()->pluck('label_id');
-                            $nodeIds = SsNodeLabel::query()->whereIn('label_id', $labels)->get()->pluck('node_id');
-                            $nodeList = SsNode::query()->whereIn('id', $nodeIds)->orderBy('sort', 'desc')->orderBy('id', 'desc')->get()->toArray();
-                            $content['serverList'] = $nodeList;
-
-                            try {
-                                Mail::to($order->email)->send(new sendUserInfo(self::$systemConfig['website_name'], $content));
-                                Helpers::addEmailLog($order->email, $title, json_encode($content));
-                            } catch (\Exception $e) {
-                                Helpers::addEmailLog($order->email, $title, json_encode($content), 0, $e->getMessage());
-                            }
-                        }
-
-                        DB::commit();
+                        $paymentComponent = new PaymentComponent($payment);
+                        $paymentComponent->paid();
                     } catch (\Exception $e) {
-                        DB::rollBack();
-
                         Log::info('【有赞云】审计订单时更新支付单和订单异常：' . $e);
                     }
                 }
@@ -502,7 +328,7 @@ class AutoJob extends Command
                     Order::query()->where('oid', $payment->oid)->update(['status' => -1]);
 
                     // 退回优惠券
-                    if ($payment->order->coupon_id) {
+                    if (!empty($payment->order) && $payment->order->coupon_id) {
                         Coupon::query()->where('id', $payment->order->coupon_id)->update(['status' => 0]);
 
                         Helpers::addCouponLog($payment->order->coupon_id, $payment->order->goods_id, $payment->oid, '订单超时未支付，自动退回');
